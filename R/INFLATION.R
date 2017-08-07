b9acb43d3f94cbbeef086380f1657584b858b5dd
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-08-07 14:20:53 -0300

    Improving documentation. Description file explains what the package is about.

--- R/INFLATION.R
+++ R/INFLATION.R
@@ -1,11 +1,28 @@
 #' @title An R package providing tools for those who want to figure out the core inflation of their series.
-#' 
+#'
+#'
+#' @description The INFLATION package provides tools that allow its user to better understand core inflation.
+#'
+#' The package provides a set of functions that compute the core inflation based on items that are part of
+#' an inflation index. Currently, the package allows for four different core inflation computations
+#' methods: trimmed means, exclusion, double weighting and triple filter. The first three are well known by the
+#' public. The latter is a method we developped that we believe is better as measure.
+#'
+#'
+#'
+#' @note The authors would like to thank the support by the Getulio
+#' Vargas Foundation (FGV) and make it clear that all
+#' data in the package is in public domain. We reaffirm that INFLATION
+#' is mainly intended for academic usage.
+#'
+#'
+#' @author Pedro Costa Ferreira \email{pedro.guilherme@fgv.br},
+#'         Talitha Speranza \email{talitha.speranza@fgv.br}
+#'         Fernando Teixeira \email{fernando.teixeira@fgv.br}
+#'
+#'
+#' @docType package
 #' @name INFLATION
-#' @description The INFLATION package...
-#' 
-#' 
-#' 
-#' 
 #' @keywords inflation bacen bcb economics core
 
 NULL
