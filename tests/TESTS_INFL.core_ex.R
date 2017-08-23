c52df39b96543e958f635ce89af9ee0e31f5ae4c
talithafs <talitha.speranza@gmail.com>
2017-08-23 03:06:38 -0300

    added tests dir and TEST_INF.core_ex

--- tests/TESTS_INFL.core_ex.R
+++ tests/TESTS_INFL.core_ex.R
@@ -15,23 +15,31 @@ ipc.ex1 <- INFL.core_ex(subitems = variacao.ts$subitens,
                         n.blocks = 4,
                         alpha = 2)
 
+## -- EXAMPLE 2
 
+# Excluding food
+weights <- pesos.ts$subitens
+weights[,substr(colnames(weights), 5,5) == 1] <- 0
 
-#' # ex2: excluindo alimentação
-#' novos_pesos <- pesos.ts$subitens
-#' novos_pesos[,substr(colnames(novos_pesos), 5,5) == 1] <- 0
-#' novos_pesos <- (novos_pesos/rowSums(novos_pesos, na.rm = TRUE))*100
-#' ipc.ex3 <- ts(rowSums(variacao.ts$subitens*novos_pesos,
-#' na.rm = TRUE)/100, start = start(variacao.ts$subitens), frequency = 12)
-#'
-#'
-#' # gráficos
-#' ts.plot(variacao.ts$ipc, ipc.ex1, col = c(1:2), lwd = 1:2)
-#' ts.plot(variacao.ts$ipc, ipc.ex3, col = c(1:2), lwd = 1:2)
-#'
-#' ipca <- ipca_get(group = "subitem")
-#' # Estes códigos ainda são do IPC - mudar para IPCA
-#' load("~/INFLATION/data/codigos.rda")
+# Rebalancing
+weights <- (weights/rowSums(novos_pesos, na.rm = TRUE))*100
+ipc.ex2 <- ts(rowSums(variacao.ts$subitens*novos_pesos, na.rm = TRUE)/100,
+          start = start(variacao.ts$subitens),
+          frequency = 12)
+
+# Plots
+ts.plot(variacao.ts$ipc, ipc.ex1, col = c(1:2), lwd = 1:2)
+ts.plot(variacao.ts$ipc, ipc.ex2, col = c(1:2), lwd = 1:2)
+
+## -- EXAMPLE 3
+
+ipca <- ipca_get(group = "subitem")
+
+# Estes códigos ainda são do IPC - mudar para IPCA
+load(paste0(getwd(),"/data/codigos.rda"))
+
 #' codigos <- codigos[1:373,]
-#' nuc <- core.ex(sub = ipca$ipca_ts, weights = ipca$weights_ts, codes = ipca$cod)
-#'
+nuc <- INFL.core_ex(subitems = ipca$ipca_ts,
+                    weights = ipca$weights_ts,
+                    info = ipca$cod)
+
