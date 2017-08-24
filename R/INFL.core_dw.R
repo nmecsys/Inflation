dbe76ede2d3fc25f28c5b8f9c4950dab8ee4bf7a
talithafs <talitha.speranza@gmail.com>
2017-08-23 23:18:03 -0300

    INFL.core_dw documented

--- R/INFL.core_dw.R
+++ R/INFL.core_dw.R
@@ -1,7 +1,7 @@
 #' @title Computes the double weighting core inflation
 #'
 #' @param infl.var A \code{ts} object. The inflation index variation.
-#' @param subit.var A \code{ts}. Subitems' variation.
+#' @param subits.var A \code{ts}. Subitems' variation.
 #' @param weights A \code{ts}. Weights corresponding to each subitem.
 #' @param wind An \code{integer}. The volatility's window size to be computed.
 #'
@@ -9,36 +9,57 @@
 #' @keywords core weight
 #' @export
 
-INFL.core_dw <- function(infl.var, subit.var, weights, wind = 48){
+INFL.core_dw <- function(infl.var, subits.var, weights, wind = 48){
 
 
-    # browser()
-    pesos <- weights
-    pi_rel <- subit.var - infl.var
-    colnames(pi_rel) <- colnames(subit.var)
+    # Build a matrix of the differences between subitems variation and total variation
+    dif <- subits.var - infl.var
+    colnames(dif) <- colnames(subits.var)
 
-    data <- as.Date(paste0(start(pi_rel)[1], "-", start(pi_rel)[2],"-01"))
-    data <- seq(data, by = "1 month", length.out = nrow(pi_rel))
-    novos_pesos <- sub*0
+    # Get starting date of inflation series
+    data <- as.Date(paste0(start(dif)[1], "-", start(dif)[2],"-01"))
+    # Create a sequence of all the dates
+    data <- seq(data, by = "1 month", length.out = nrow(dif))
 
-    for(t in nrow(pi_rel):wind){
+    # Create an empty vector of size = length(subits.var)
+    db.weights <- subits.var*0
 
-        ano1 <- as.numeric(substr(data[t-wind+1],1,4))
-        mes1 <- as.numeric(substr(data[t-wind+1],6,7))
+    # Loop over moving windows
+    for(t in nrow(dif):wind){
 
-        ano2 <- as.numeric(substr(data[t],1,4))
-        mes2 <- as.numeric(substr(data[t],6,7))
+        # Get window's starting year and month
+        start.year <- as.numeric(substr(data[t-wind+1],1,4))
+        start.mth <- as.numeric(substr(data[t-wind+1],6,7))
 
-        sub_pi_rel <- window(pi_rel, start = c(ano1,mes1), end = c(ano2,mes2), frequency = 12)
-        desvios <- apply(sub_pi_rel, MARGIN = 2, FUN = sd, na.rm = T)
-        novos_pesos[t,] <- ((1/desvios)/sum(1/desvios, na.rm = T))*pesos[t,]
+        # Get window's ending year and month
+        end.year <- as.numeric(substr(data[t],1,4))
+        end.mth <- as.numeric(substr(data[t],6,7))
+
+        # Get window
+        dif.wind <- window(dif, start = c(start.year,start.mth), end = c(end.year,end.mth), frequency = 12)
+        # Calculate standard deviations of the differences
+        std.devs <- apply(dif.wind, MARGIN = 2, FUN = sd, na.rm = T)
+
+        # Normalize the inverse of the standard deviations to obtain new weights
+        # Intuition: the larger the sd, the smaller should be the weight
+        # Multiply new weights by old weights (hence double weighting)
+        db.weights[t,] <- ((1/std.devs)/sum(1/std.devs, na.rm = T))*weights[t,]
     }
 
-    novos_pesos2 <- window(novos_pesos, start = c(as.numeric(substr(data[wind+1],1,4)), as.numeric(substr(data[wind+1],6,7))),
-                           frequency = 12)
-    novos_pesos3 <- novos_pesos2/rowSums(novos_pesos2,na.rm = T)
-    sub2 <- window(sub, start = start(novos_pesos2), frequency = 12)
-    core <- ts(rowSums(novos_pesos3*sub2, na.rm = T), start = start(sub2), frequency = 12)
+
+    # Cut first 'wind' rows from db.weights (rows filled with 0s)
+    # !!-- PROBABLY REDUNDANT
+    dw.start = c(as.numeric(substr(data[wind+1],1,4)), as.numeric(substr(data[wind+1],6,7)))
+    db.weights <- window(db.weights, start = dw.start, frequency = 12)
+
+    # Rebalance weights
+    db.weights <- db.weights/rowSums(db.weights,na.rm = T)
+
+    # Cut first 'wind' rows from subits.var (there are no double weights for these rows)
+    subits.var <- window(subits.var, start = dw.start, frequency = 12)
+
+    # Compute weighted sum of all items to obtain the core
+    core <- ts(rowSums(db.weights*subits.var, na.rm = T), start = start(subits.var), frequency = 12)
     core
 }
 
