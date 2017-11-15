3d4c92b9869ac65c981b7d0f633b4a4fe6cde783
talithafs <talitha.speranza@gmail.com>
2017-11-15 11:21:47 -0200

    infl.bias_test finished

--- R/infl.bias_test.R
+++ R/infl.bias_test.R
@@ -4,7 +4,15 @@
 #' @return
 #'
 #' @keywords
+#'
+#' @import dymlm
 #' @export
 #' @examples
 #'
 #' @author Talitha Speranza \email{talitha.speranza@fgv.br}
+
+
+infl.bias_test <- function(index,core){
+
+    coeffs <- summary(dynlm(index ~ 1 + core))$coefficients
+}
