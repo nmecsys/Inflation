db2d0077663140eecc852cab5b3bc094b5ff251d
talithafs <talitha.speranza@gmail.com>
2017-11-15 10:24:40 -0200

    infl.desc_stat concluida

--- R/infl.qs_test.R
+++ R/infl.qs_test.R
@@ -7,4 +7,25 @@
 #' @export
 #' @examples
 #'
+#' @importFrom seasonal seas qs
+#'
 #' @author Talitha Speranza \email{talitha.speranza@fgv.br}
+
+
+infl.qs_test <- function(index, core, nms = NULL){
+
+    stats <- data.frame(matrix(ncol = 2, nrow = 2))
+
+    indx.qs <- qs(seas(index))
+    core.qs <- qs(seas(core))
+
+    if(!is.null(nms)){
+        rownames(stats) <- nms
+    } else {
+        rownames(stats) <- c("Index","Core")
+    }
+
+    colnames(stats) <- c("Statistic","P.Value")
+
+
+}
