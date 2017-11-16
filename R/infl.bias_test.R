8b44a44d1214648df8421678b996f2740bc7985d
talithafs <talitha.speranza@gmail.com>
2017-11-16 16:42:52 -0200

    infl.fit_dyn finished

--- R/infl.bias_test.R
+++ R/infl.bias_test.R
@@ -15,4 +15,5 @@
 infl.bias_test <- function(index,core){
 
     coeffs <- summary(dynlm(index ~ 1 + core))$coefficients
+    return(coeffs)
 }
--- R/infl.fit_dyn.R
+++ R/infl.fit_dyn.R
@@ -4,7 +4,37 @@
 #' @return
 #'
 #' @keywords
+#'
+#' @import dynlm
+#' @importFrom vars VARselect VAR
 #' @export
 #' @examples
 #'
 #' @author Talitha Speranza \email{talitha.speranza@fgv.br}
+
+infl.fit_dyn <- function(index, core, criterion = "SC"){
+
+    dlm <- dynlm(index ~ 1 + core)
+    u = lag(dlm$residuals,1)
+
+    d.index = diff(index)
+    d.core = diff(core)
+
+    mat = na.omit(cbind(u,d.index,d.core))
+
+    sel = VARselect(mat[,-1], type = "const", exogen = mat[,1])
+
+    if(criterion = "SC"){
+        lags = as.integer(sel$selection[3])
+    } else if(criterion  = "AIC"){
+        lags = as.integer(sel$selection[1])
+    } else if(criterion = "HQ"){
+        lags = as.integer(sel$selection[2])
+    } else {
+        lags = as.integer(sel$selection[4])
+    }
+
+    var <- VAR(mat[,-1], p = lags, type = "const", exogen = mat[,1])
+
+    return(var)
+}
