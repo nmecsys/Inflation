83e0aa0ecdabd42a632cf7ed8222819a3c123bb0
talithafs <talitha.speranza@gmail.com>
2017-11-16 17:30:38 -0200

    infl.fit_dyn improved

--- R/infl.fit_dyn.R
+++ R/infl.fit_dyn.R
@@ -6,23 +6,30 @@
 #' @keywords
 #'
 #' @import dynlm
-#' @importFrom vars VARselect VAR
+#' @import vars
 #' @export
 #' @examples
 #'
 #' @author Talitha Speranza \email{talitha.speranza@fgv.br}
 
-infl.fit_dyn <- function(index, core, criterion = "SC"){
+infl.fit_dyn <- function(index, core, f = 1, dif = T, criterion = "SC"){
 
     dlm <- dynlm(index ~ 1 + core)
-    u = lag(dlm$residuals,1)
+    u = lag(dlm$residuals,f)
 
-    d.index = diff(index)
-    d.core = diff(core)
+    d.index = diff(index, f)
+    d.core = diff(core, f)
 
     mat = na.omit(cbind(u,d.index,d.core))
 
-    sel = VARselect(mat[,-1], type = "const", exogen = mat[,1])
+    if(dif){
+        mat = na.omit(cbind(index - core, mat))
+        colnames(mat) <- c("dif","u","d.index","d.core")
+        sel = VARselect(mat[,c(-1,-2)], type = "const", exogen = mat[,1:2])
+
+    } else {
+        sel = VARselect(mat[,-1], type = "const", exogen = mat[,1])
+    }
 
     if(criterion = "SC"){
         lags = as.integer(sel$selection[3])
@@ -34,7 +41,15 @@ infl.fit_dyn <- function(index, core, criterion = "SC"){
         lags = as.integer(sel$selection[4])
     }
 
-    var <- VAR(mat[,-1], p = lags, type = "const", exogen = mat[,1])
+    if(dif){
+        var <- VAR(mat[,c(-1,-2)], p = lags, type = "const", exogen = mat[,1:2])
+    } else {
+        var <- VAR(mat[,-1], p = lags, type = "const", exogen = mat[,1])
+    }
+
+    var$arch = arch.test(var)
+    var$autocorr = serial.test(var)
+    var$stab = roots(var)
 
     return(var)
 }
